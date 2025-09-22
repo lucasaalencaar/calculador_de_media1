21790e6bfa6dda3f3dfd5273d44df5052fba04bb
Lucas de Alencar <lucasalvesalencar3@gmail.com>
2025-09-22 18:29:41 -0300

    Add files via upload
    script que guarda as notas do aluno e calcula suas médias com o método aritmético e ponderado

---
+++ calculador_de_médias.py
@@ -0,0 +1,13 @@
+from ex007 import media_aritmetica, media_ponderada
+
+nome = input('Nome do aluno: ')
+print('Digite suas notas', nome)
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+nota3 = float(input('Digite a terceira nota: '))
+media_aritmedica = (nota1 + nota2 + nota3) / 3
+media_ponderada = (nota1 * 2 + nota2 * 3 + nota3 * 5 ) / 10
+print(f'Notas: {nota1}, {nota2}, {nota3} ')
+print(f'Média aritmética: {media_aritmedica}')
+print(f'Média ponderada: {media_ponderada}')
+
