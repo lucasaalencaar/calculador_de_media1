b5cdc13dd175304486bdfeef73b167b349acc63f
Lucas de Alencar <lucasalvesalencar3@gmail.com>
2025-09-29 16:31:29 -0300

    Update calculador_de_médias.py

--- calculador_de_médias.py
+++ calculador_de_médias.py
@@ -1,4 +1,4 @@
-from ex007 import media_aritmetica, media_ponderada
+
 
 nome = input('Nome do aluno: ')
 print('Digite suas notas', nome)
@@ -10,4 +10,5 @@ media_ponderada = (nota1 * 2 + nota2 * 3 + nota3 * 5 ) / 10
 print(f'Notas: {nota1}, {nota2}, {nota3} ')
 print(f'Média aritmética: {media_aritmedica}')
 print(f'Média ponderada: {media_ponderada}')
+
 
